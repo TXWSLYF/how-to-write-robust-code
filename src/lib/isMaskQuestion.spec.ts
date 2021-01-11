8e591a21b9682a26f0e1fcd1e67b763eaa56d66a
alex <1060319328@qq.com>
2021-01-11 16:34:51 +0800

    feat: add isMaskQuestion.spec.ts

---
+++ src/lib/isMaskQuestion.spec.ts
@@ -0,0 +1,22 @@
+import test from 'ava';
+
+import { isMaskQuestion } from './isMaskQuestion';
+
+const selectQuestionExt = '{"template":{"bundleName":"selectQuestion"}}';
+const maskAudioQuestionExt = '{"template":{"bundleName":"maskAudioQuestion"}}';
+const maskChoiceQuestionExt =
+  '{"template":{"bundleName":"maskChoiceQuestion"}}';
+
+test('isMaskQuestion', (t) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  t.is(isMaskQuestion(undefined), false);
+
+  t.is(isMaskQuestion('sss'), false);
+
+  t.is(isMaskQuestion(selectQuestionExt), false);
+
+  t.is(isMaskQuestion(maskAudioQuestionExt), true);
+
+  t.is(isMaskQuestion(maskChoiceQuestionExt), true);
+});
