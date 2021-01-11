0207597aefdcd3fe2d7e6f934b8b8c406242ec8a
alex <1060319328@qq.com>
2021-01-11 17:39:04 +0800

    feat: isMaskQuestion 代码实现

--- src/lib/isMaskQuestion.ts
+++ src/lib/isMaskQuestion.ts
@@ -1,3 +1,26 @@
+type Question = {
+  readonly template: {
+    readonly bundleName: string;
+  };
+};
+
+const MASK_BUNDLE_NAMES = ['maskChoiceQuestion', 'maskAudioQuestion'];
+
 export const isMaskQuestion = (ext: string): boolean => {
-  return !!ext;
+  // 运行时校验
+  if (typeof ext !== 'string') return false;
+
+  // JSON.parse 可能会抛出错误
+  try {
+    const question: Question = JSON.parse(ext);
+
+    if (!question || !question.template || !question.template.bundleName)
+      return false;
+
+    return !!MASK_BUNDLE_NAMES.find(
+      (name) => name === question.template.bundleName
+    );
+  } catch {
+    return false;
+  }
 };
